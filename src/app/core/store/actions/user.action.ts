20e19d786c8abe189e7c0cf33090a887a205676d
Alaa Nassar <alaanassar1211@gmail.com>
2023-11-05 05:20:23 +0200

    finish store

--- src/app/core/store/actions/user.action.ts
+++ src/app/core/store/actions/user.action.ts
@@ -2,14 +2,14 @@ import { createAction, props } from '@ngrx/store';
 
 export const loadUsers = createAction('[User] Load Users');
 export const updateQuery = createAction(
-  '[User] Update Query',
+  'Update Query',
   props<{ query: string }>()
 );
 export const loadUsersSuccess = createAction(
-  '[User] Load Users Success',
+  'Load Users Success',
   props<{ users: any[] }>()
 );
 export const loadUsersFailure = createAction(
-  '[User] Load Users Failure',
+  'Load Users Failure',
   props<{ error: string }>()
 );
